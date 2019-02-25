38b717aaaab0f54f1df81f6fca0348f51c7b8374
unknown <lincoln.choy1997@gmail.com>
2019-02-26 03:18:44 +1300

    Styling changes

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -43,14 +43,14 @@ const styles = StyleSheet.create({
 	listItem: {
 		width: "100%",
 		marginBottom: 5,
-		padding: 10,
+		padding: 5,
 		backgroundColor: "#eee",
 		flexDirection: "row",
 		alignItems: "center"
 	},
 	placeImage: {
-			marginRight: 8,
-			height: 30,
-			width: 30
+		marginRight: 8,
+		height: 50,
+		width: 50
 	}
 });
