24008d2319506adea729fda13e7ae1b6bfd302f5
unknown <lincoln.choy1997@gmail.com>
2019-03-06 01:49:45 +1300

    Added map and marker

--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -1,21 +1,60 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
-
-
+import { View, Button, StyleSheet, Dimensions } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 class PickLocation extends React.Component {
 
-    
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            focusedLocation: {
+                latitude: 37.7900352,
+                longitude: -122.4013726,
+                latitudeDelta: 0.0122,
+                longitudeDelta: (Dimensions.get('window').width/ Dimensions.get('window').height )* 0.0122 
+            },
+            locationChosen: false
+        }
+    }
+
+
+    pickLocation = (event) => {
+
+        const coords = event.nativeEvent.coordinate;
+
+        this.setState(prevState => {
+            return {
+                focusedLocation: {
+                    ...prevState.focusedLocation,
+                    latitude: coords.latitude,
+                    longitude: coords.longitude
+                },
+                locationChosen: true
+            }
+        })
+    }
 
     render() {
 
+        let marker = null;
+
+        if (this.state.locationChosen) {
+            marker = 
+                <MapView.Marker coordinate = {this.state.focusedLocation}/>
+        }
         return (
             <View style = {styles.container }>
-                <View style = {styles.placeHolder}>
-                    <Text>
-                        Map
-                    </Text>
-                </View>
+                <MapView 
+                    region = {this.state.focusedLocation}
+                    provider = {PROVIDER_GOOGLE} 
+                    style = {styles.map}
+                    onPress = {this.pickLocation}
+                >
+                    { marker }
+                </MapView>
                 <View style = {styles.button}>
                     <Button title = 'Locate me' onPress = { () => { alert('Pick location') }}/>
                 </View>
@@ -33,13 +72,9 @@ const styles = StyleSheet.create({
         width: '100%',
         alignItems: 'center'
     },
-    placeHolder: {
-        margin: 5,
-        borderWidth: 1,
-        borderColor: 'black',
-        backgroundColor: '#eee',
-        width: '80%',
-        height: 150
+    map: {
+        width: '100%',
+        height: 250
     },
     button: {
         margin: 8
